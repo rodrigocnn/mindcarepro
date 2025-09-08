e8cfb91a9b2ea00c31aa4648db5b31d113a5eeba
Rodrigo César <rodrigocnn@gmail.com>
2025-09-08 20:32:19 -0300

    chore: add autRoutes

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { patientRoutes } from "../../modules/patient/routes/patient.routes";
 import psychologistsRoutes from "../../modules/psychologist/routes/psychologist.routes";
+import authRoutes from "../../modules/auth/routes/auth.routes";
 
 export const router = Router();
 
+router.use(authRoutes);
 router.use(psychologistsRoutes);
 router.use(patientRoutes);
